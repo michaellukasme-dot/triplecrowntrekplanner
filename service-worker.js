578e7b2a52430f1ccdb364eb5f40d3485df07e48
michaellukasme-dot <michaellukas.me@gmail.com>
2026-06-02 20:38:24 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
  * ----------------------------------------------------------------------------
  */
 
-const CACHE_VERSION = 'v4';                       // ⬅ bump on meaningful asset changes
+const CACHE_VERSION = 'v5';                       // ⬅ bump on meaningful asset changes
 const SHELL_CACHE  = 'tctp-shell-'  + CACHE_VERSION;
 const ASSET_CACHE  = 'tctp-assets-' + CACHE_VERSION;
 
